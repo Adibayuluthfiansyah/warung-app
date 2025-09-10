dd1b574cd410ff7a301267cc82e0130cf721abe4
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-09-11 00:13:00 +0700

    test add card and setup db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import React from "react"
+import { Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,} from "@/components/ui/card"
 
 export default function Home() {
   return (
-    <div className="text-center pt-80">
+    <div className="text-center">
       <h1 className="text-bold text-5xl">This Main Page</h1>
-      <h2>This is second page</h2>
+      <div className="pt-5">
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+              <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+      </div>
     </div>
   )
 }
