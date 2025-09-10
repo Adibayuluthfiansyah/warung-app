024e46c1aec8dc35a0c6f04d06af184ec83e0d00
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-09-11 00:05:46 +0700

    set up api supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="text-center pt-80">
       <h1 className="text-bold text-5xl">This Main Page</h1>
+      <h2>This is second page</h2>
     </div>
   )
 }
