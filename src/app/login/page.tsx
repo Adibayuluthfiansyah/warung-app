aca6bbfac467b3136e7650af6aba8aeccdd0eaab
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-09-12 21:26:27 +0700

    add login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,19 @@
 'use client'
-
 import React from 'react'
+import { LoginForm } from '@/components/login-form'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+        ANAKIN SHOP
+        </a>
+        <LoginForm />
+      </div>
+    </div>
   )
 }
 
-export default LoginPage 
+
+export default LoginPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './navbar/page'
+import LoginPage from './login/page'
 
 
 const Home = () => {
   return (
     <div className='text-center'>
       <Navbar/>
+      <LoginPage/>
       <h1>This Is Home Page</h1>
     </div>
   )
