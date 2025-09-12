5f47c9543e2b3d7234d9b266c18102c3c700a66f
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-09-12 20:56:30 +0700

    add login page and navbar use shadcn and origin ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
-'use client'
-import React, { useEffect, useState } from "react"
-import type { Menu } from "@/lib/db-types";
-import supabase from "@/lib/db";
+import React from 'react'
 
-
-export default function Home() {
-  const [menu, setMenus] = useState<Menu[]>([]);
-  
-  useEffect(() => {
-    const fetchMenus = async () => {
-      const {data, error} = await supabase.from('menu').select('*');
-      if (error) console.log('error:', error)
-        else setMenus(data);
-    };
-  fetchMenus();
-  },[supabase]);
+const Home = () => {
   return (
-    <div className="text-center">
-      <h1 className="text-bold text-5xl">This Main Page</h1>
-      <div className="pt-5">
-      </div>
+    <div className='text-center'>
+      <h1>This Is Home Page</h1>
     </div>
   )
 }
+
+export default Home
